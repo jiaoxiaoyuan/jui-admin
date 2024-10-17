08c15a84a867cea824a101c9188ca305782b652d
MrJiao <329492979@qq.com>
2024-10-18 00:41:43 +0800

    ✨ feat: 优化

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    // const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(), {
+        cors: true,
+    });
 
-    const options = new DocumentBuilder()
-        .addBearerAuth() // token
-        .setTitle('Cats API')
-        .setDescription('The cats API description')
-        .setVersion('1.0')
-        .build();
+    // const options = new DocumentBuilder()
+    //     .addBearerAuth() // token
+    //     .setTitle('Cats API')
+    //     .setDescription('The cats API description')
+    //     .setVersion('1.0')
+    //     .build();
 
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('/api-doc', app, document);
+    // const document = SwaggerModule.createDocument(app, options);
+    // SwaggerModule.setup('/api-doc', app, document);
+
+    // 注册 Swagger 的配置顺序
+    setupSwagger(app);
 
     console.log('http://localhost:3001/api-doc');
     await app.listen(3001);
---
+++ server/src/swagger/index.ts
@@ -0,0 +1,14 @@
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export const setupSwagger = (app: NestExpressApplication) => {
+    // swagger
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('梦享时间 API')
+        .setDescription('梦享时间 backend API')
+        .setVersion('1.0')
+        .addTag('beta')
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('/api-doc', app, document);
+};
