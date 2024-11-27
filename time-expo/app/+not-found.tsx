4c428ea8431b4a6cc520cfb35429e2b4610468eb
NativeBase <329492979@qq.com>
2024-11-28 00:54:47 +0800

    📃 docs:

--- time-expo/app/+not-found.tsx
+++ time-expo/app/+not-found.tsx
@@ -1,32 +1,36 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router'
+import { View, StyleSheet } from 'react-native'
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+    return (
+        <>
+            <Stack.Screen options={{ title: 'Oops!' }} />
+            <View style={styles.container}>
+                <Link href='/' style={styles.button}>
+                    没有内容
+                </Link>
+            </View>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#fff'
+    }
+})
--- time-expo/app/_layout.tsx
+++ time-expo/app/_layout.tsx
@@ -27,11 +27,19 @@ export default function RootLayout() {
     }
 
     return (
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <Stack>
-                <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-                <Stack.Screen name='+not-found' />
-            </Stack>
-        </ThemeProvider>
+        <Stack
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#ffffff'
+                },
+                headerTintColor: '#000000',
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                }
+            }}
+        >
+            <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+            <Stack.Screen name='+not-found' />
+        </Stack>
     )
 }
