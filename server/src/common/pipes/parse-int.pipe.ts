54c3464b558555fd6f538aaf629177c1f2cfc162
NativeBase <329492979@qq.com>
2024-12-17 00:22:25 +0800

    📃 docs:

--- server/src/common/pipes/parse-int.pipe.ts
+++ server/src/common/pipes/parse-int.pipe.ts
@@ -4,6 +4,8 @@ import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from
 export class ParseIntPipe implements PipeTransform<string> {
     async transform (value: string, metadata: ArgumentMetadata) {
         const val = parseInt(value, 10);
+
+        console.log('metadata', metadata)
         if (isNaN(val)) {
             throw new BadRequestException('Validation failed');
         }
