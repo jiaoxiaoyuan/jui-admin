6c37af91c6419860306e4393e71a3dd8ff417938
NativeBase <329492979@qq.com>
2024-12-17 00:14:09 +0800

    📃 docs:

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -16,7 +16,8 @@ import { UserModule } from './modules/user/user.module';
 })
 export class AppModule {
     configure (consumer: MiddlewareConsumer) {
-        // 为 hello 路由添加中间件
+
+        // 为 user 路由添加中间件
         consumer
             .apply(LoggerMiddleware)
             .exclude({ path: 'user', method: RequestMethod.POST })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { ExpressAdapter } from '@nestjs/platform-express';
-
 import { ValidationPipe } from '@nestjs/common';
 
-
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 import { setupSwagger } from './swagger';
 
 async function bootstrap () {
-    // const app = await NestFactory.create(AppModule);
     const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(), {
         cors: true,
     });
@@ -20,8 +16,12 @@ async function bootstrap () {
     // 全局管道
     app.useGlobalPipes(new ValidationPipe());
 
+    // 全局过滤器
+    app.useGlobalFilters(new HttpExceptionFilter());
+
 
     //  app.setGlobalPrefix('api'); // 配置全局前缀
+
     // 注册 Swagger 的配置顺序
     setupSwagger(app);
 
