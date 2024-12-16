8e47e2392e805d6430d87954455f012d90f12fb3
NativeBase <329492979@qq.com>
2024-12-16 21:18:09 +0800

    📃 docs: 用户模块

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service'
 
 // 友情链接
 import { LinksModule } from './modules/links/links.module'
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-	imports: [LinksModule],
+	imports: [LinksModule, UserModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    
+}
